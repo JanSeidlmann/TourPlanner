7ec2d506f3692b19f76e0eeace379dad9e4cf850
laura <laura.engleitner@gmail.com>
2024-04-29 17:05:52 +0200

    change tour-planner.fxml to a grid

--- src/main/java/org/example/tourplanner/HelloApplication.java
+++ src/main/java/org/example/tourplanner/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tour-planner.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setResizable(false);
+        stage.setResizable(true);
         stage.setTitle("Tour Planner");
         stage.setScene(scene);
         stage.show();
