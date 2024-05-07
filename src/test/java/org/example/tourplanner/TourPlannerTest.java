acb6b98186f7607b052a03ab78c26fcc8a572347
laura <laura.engleitner@gmail.com>
2024-05-07 09:51:47 +0200

    add testClass

---
+++ src/test/java/org/example/tourplanner/TourPlannerTest.java
@@ -0,0 +1,19 @@
+package org.example.tourplanner;
+
+import org.junit.jupiter.api.Test;
+
+public class TourPlannerTest {
+
+
+    @Test
+    void testAddTourButtonEnabled() {
+        //verifyThat("#addTourButton", isEnabled());
+    }
+
+    @Test
+    void testAddNameToTour() {
+        //clickOn("").write("");
+
+        //verifyThat("#cancelButton", isVisible());
+    }
+}
